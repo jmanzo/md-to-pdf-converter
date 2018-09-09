1f866c69c016f55dd020e4d19e669b4fc9b52b9e
jmanzo <jmanzosistemas@gmail.com>
2018-09-09 16:53:19 -0500

    converting links to C sintax for markdown-include library

--- markdown-pdf.js
+++ markdown-pdf.js
@@ -1,17 +1,40 @@
 var fs = require('fs'),
     path = require('path'),
     md = require("markdown").markdown,
+    markdownpdf = require("markdown-pdf"),
     filePath = path.join(__dirname, 'SUMMARY.md');
 
 fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
     if (!err) {
-    	console.log(data);
-        //parsingMD(data);
+        parsingMD(data);
     } else {
         console.log(err);
     }
 });
 
-function parsingMD(file) {
-	//
+function parsingMD(data) {
+	var look = data.split(/\n/);
+	var re = /\(([^)]+)\)/;
+	var reBrackets = /\[([^)]+)\]/;
+
+	for (var i = 0; i < look.length; i++) {
+		var replacement = re.exec(look[i]);
+		var replaceBrackets = reBrackets.exec(look[i]);
+		
+		if (replacement) {
+			look[i] = look[i].replace(re, '#include "' + replacement[1] + '"');
+			look[i] = look[i].replace(reBrackets, '');
+		}
+	}
+
+	stringToMD(look);
+}
+
+function stringToMD(data) {
+	var outputPath = "SUMMARY.pdf",
+		output = data.toString().replace(/,/g, '\n');
+
+	markdownpdf().from.string(output).to(outputPath, function () {
+	  console.log("Created", outputPath)
+	})
 }
