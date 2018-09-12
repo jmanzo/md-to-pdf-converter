5390b40b525ed794e380098a467ef5a16ebe2fc6
jmanzo <jmanzosistemas@gmail.com>
2018-09-12 17:09:14 -0500

    mvp application

--- markdown-pdf.js
+++ markdown-pdf.js
@@ -1,7 +1,10 @@
 var fs = require('fs'),
     path = require('path'),
     markdownpdf = require("markdown-pdf"),
-    filePath = path.join(__dirname, './output/SUMMARY.md');
+  	through = require("through2"),
+  	cheerio = require('cheerio'),
+    filePath = path.join(__dirname, 'output/SUMMARY.md'),
+    basePath = path.resolve(__dirname, 'output/SUMMARY.md');
 
 fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
     if (!err) {
@@ -16,44 +19,43 @@ function parsingMD(data) {
 		re = /\(([^)]+)\)/,
 		reBrackets = /\[([^)]+)\]/,
 		mdDocs = new Array(),
-		bookPath = './output/SUMMARY.pdf',
-		replaceLink;
+		bookPath = 'output/SUMMARY.pdf';
 
 	for (var i = 0; i < look.length; i++) {
 		var replacement = re.exec(look[i]);
 		
 		if (replacement) {
-			look[i] = look[i].replace(re, '#include "' + replacement[1] + '"');
+			look[i] = look[i].replace(re, '#include "output/' + replacement[1] + '"');
 			look[i] = look[i].replace(reBrackets, '');
 			look[i] = look[i].replace('*', '');
 			look[i] = look[i].trim();
 
-			var dirFile = replacement[1],
-				reImg = /(!\[.*?\]\()(.+?)(\))/g,
-				replaceLink = dirFile.split('/');
-
-			if (replaceLink.length > 1) {
-				fs.readFile(dirFile, {encoding: 'utf-8'}, function (err,data) {
-				  if (err) {
-				    return console.log(err);
-				  }
-
-				  var result = data.replace(reImg, function(whole, a, b, c){
-				  	b = b.replace('../', '');
-				  	return a + path.resolve(__dirname, b) + c;
-				  });
-
-				  fs.writeFile(dirFile, result, {encoding: 'utf-8'}, function (err) {
-				     if (err) return console.log(err);
-				  });
-				});
-			}
-
-			mdDocs.push(replacement[1]);
+			mdDocs.push('output/' + replacement[1]);
 		}
 	}
-	
-	markdownpdf().concat.from(mdDocs).to(bookPath, function () {
-		console.log("Created", bookPath)
+
+	markdownpdf({ preProcessHtml:preProcessHtml(basePath) }).concat.from(mdDocs).to(bookPath, function () {
+		console.log("PDF file created: ", bookPath)
 	})
+}
+
+function preProcessHtml (basePath) {
+  	return function() {
+        return through(function(chunk, encoding, callback) {
+            var $ = cheerio.load(chunk);
+
+            $('img[src]').each(function() {
+                var imagePath = $(this).attr('src');
+                imagePath = path.resolve(basePath, imagePath);
+
+                $(this).attr('src', 'file://' + (process.platform === 'win32' ? '/' : '') + imagePath);
+            });
+
+            this.push($.html());
+
+            console.log("Html file processed.");
+
+            callback();
+        });
+    }
 }
